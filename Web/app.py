360ca1ad326133e2bc60aa0667415754d63aad2c
ZnakeDenmark <Victor.dyhrman@gmail.com>
2023-09-12 08:16:47 +0200

    Changed the whole machine

--- Web/app.py
+++ Web/app.py
@@ -4,17 +4,18 @@ import pickle
 
 app = Flask(__name__)
 
+#Vi loader vores pickle med pickle filen som vi lavet i jupyter notebook
 model = pickle.load(open("./Job.pkl", "rb"))
 
-
+#Vi loader home med template test.html
 @app.route("/")
 def home():
     return render_template("test.html")
 
-
+#This is the function to predict the outcome
 @app.route("/predict", methods=["POST"])
 def predict():
-
+    
     int_features = [int(x) for x in request.form.values()]
     print(int_features)
     final_features = [np.array(int_features)]
